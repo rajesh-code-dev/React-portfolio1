2f7d572dd9967612fbe80a704a6b0018dc332b2b
Rajesh Chaudhary <114743505+rajesh7688@users.noreply.github.com>
2023-10-09 01:30:28 +0530

    dd

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,68 @@
-import { useRef } from 'react';
-import './header.css'
+import { useRef, useState} from "react";
+import "./header.css";
 import { FaBars, FaTimes } from "react-icons/fa";
 const Header = () => {
   const navRef = useRef();
   const showNavBar = () => {
-    navRef.current.classList.toggle("responsive_nav")
-  }
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  const [txtTransform, setTxtTransform] = useState();
+  window.addEventListener('load', () => {
+    const txtAnimate = localStorage.getItem("txtAnimate");
+    setTxtTransform(txtAnimate)
+  })
+ 
+
   return (
-    <div className='navbarContainer'>
+    <div className="navbarContainer">
       <div className="wrap ">
-        <button className='nav-btn right'onClick={showNavBar} >
-          <FaBars/>
+        <button className="nav-btn right" onClick={showNavBar}>
+          <FaBars />
         </button>
       </div>
-      <div className='navbar'>
-      <h5>MODERN WEB</h5>
+      <div className="navbar">
+        <div className="bounding">
+          <h5
+            className="boundingtxt"
+            style={{
+              transform: `translateY(${txtTransform})`,
+              transition: "1s",
+            }}
+          >
+            MODERN WEB
+          </h5>
+        </div>
         <nav className="cont" ref={navRef}>
-          
-          <ul className='link'>
-            <a className='nav-text' ref={navRef} href="#about">About</a>
-            <a className='nav-text' href="#skill">Skill</a>
-            <a className='nav-text' href="#projects">Project</a>
-            <a className='nav-text' href="#contect">Contact</a>
-            <button className='nav-btn close-nav-btn' onClick={showNavBar}>
+          <ul className="link">
+            <div className="bounding1">
+              <a className="nav-text boundingtxt" ref={navRef} href="#about">
+                About
+              </a>
+            </div>
+            <div className="bounding1">
+              <a className="nav-text boundingtxt" href="#skill">
+                Skill
+              </a>
+            </div>
+            <div className="bounding1">
+              <a className="nav-text boundingtxt" href="#projects">
+                Project
+              </a>
+            </div>
+            <div className="bounding1">
+              <a className="nav-text boundingtxt" href="#contect">
+                Contact
+              </a>
+            </div>
+            <button className="nav-btn close-nav-btn" onClick={showNavBar}>
               <FaTimes />
             </button>
           </ul>
         </nav>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,25 +3,46 @@ import { FaArrowUp } from "react-icons/fa";
 import "./home.css";
 import img from "../home/img1.jpeg";
 const Home = () => {
+  const [txtTransform, setTxtTransform] = useState();
+  window.addEventListener("load", (event) => {
+    setTxtTransform("0");
+  });
 
-  const [txtTransform, setTxtTransform] = useState()
-  window.addEventListener('load', (event) => {
-    setTxtTransform('0')
-  })
+  localStorage.setItem('txtAnimate', txtTransform)
 
   return (
     <>
       <div className="home" id="home1">
         <div className="home-text">
           <div className="bounding">
-            <h1 className={`home-text-h1`} style={{transform: `translateY(${txtTransform})`, transition: '1s'}}>Hi, I'm </h1>
+            <h1
+              className={`home-text-h1`}
+              style={{
+                transform: `translateY(${txtTransform})`,
+                transition: "1s",
+              }}
+            >
+              Hi, I'm{" "}
+            </h1>
           </div>
           <div className="bounding">
-            <h1 className="home-text1-h1" style={{transform: `translateY(${txtTransform})`, transition: '1s', transitionDelay: '.5s'}}>Rajesh.</h1>
+            <h1
+              className="home-text1-h1"
+              style={{
+                transform: `translateY(${txtTransform})`,
+                transition: "1s",
+                transitionDelay: ".5s",
+              }}
+            >
+              Rajesh.
+            </h1>
           </div>
           <div className="animate">
-            <p className="home-text-h3">I build amazing things for the web.</p>
-            <p className="home-text-h3">I build amazing things for the web.</p>
+            <p className="endtxt"  style={{
+                transform: `translateY(${txtTransform})`,
+                transition: "1s",
+                transitionDelay: ".8s",
+              }}>I build amazing things for the web.</p>
           </div>
           {/* <div className="home-btn" >
             <a href="#contect" className='btn-text'>Contact me.</a>
@@ -41,11 +62,8 @@ const Home = () => {
             Stack enthusiast, JavaScript lover.
           </p>
         </div>
-      </div>
-
-      <div className="arrow">
-        <div>
-
+        <div className="arrow">
+          <div>Contact Me.</div>
         </div>
       </div>
     </>
